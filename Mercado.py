32e707c89018dae3bc4b7273c52731cd82dacd85
GABRIEL HANSEN DE VASCONCELOS <72347313+Gabriel-Hansen@users.noreply.github.com>
2023-05-12 19:45:17 -0300

    Update Mercado.py
    Comentando o crud

--- Mercado.py
+++ Mercado.py
@@ -1,3 +1,5 @@
+#Primeiro arquivo de teste
+
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
@@ -49,61 +51,61 @@ Base.metadata.create_all(engine)
 
 #create
 
-from sqlalchemy.orm import sessionmaker
+#from sqlalchemy.orm import sessionmaker8
 
-Session = sessionmaker(bind=engine)
-session = Session()
+#Session = sessionmaker(bind=engine)
+#session = Session()
 
-novo_item = Item(nome="Arroz", codigo_barras="7891234567890", preco=500, quantidade_estoque=100, fornecedor_id=1)
-session.add(novo_item)
-session.commit()
+#novo_item = Item(nome="Arroz", codigo_barras="7891234567890", preco=500, quantidade_estoque=100, fornecedor_id=1)
+#session.add(novo_item)
+#session.commit()
 
-novo_fornecedor = Fornecedor(nome="Fornecedor A", endereco="Rua 123, Bairro C", itens=[novo_item])
-session.add(novo_fornecedor)
-session.commit()
+#novo_fornecedor = Fornecedor(nome="Fornecedor A", endereco="Rua 123, Bairro C", itens=[novo_item])
+#session.add(novo_fornecedor)
+#session.commit()
 
 
-novo_funcionario = Funcionario(nome="João", cpf="12345678901", cargo="Atendente", senha="1234")
-session.add(novo_funcionario)
-session.commit()
+#novo_funcionario = Funcionario(nome="João", cpf="12345678901", cargo="Atendente", senha="1234")
+#session.add(novo_funcionario)
+#session.commit()
 
 
 #read
 
-itens = session.query(Item).all()
-for item in itens:
-    print(item.nome, item.codigo_barras, item.preco, item.quantidade_estoque)
+#itens = session.query(Item).all()
+#for item in itens:
+   # print(item.nome, item.codigo_barras, item.preco, item.quantidade_estoque)
 
-fornecedores = session.query(Fornecedor).all()
-for fornecedor in fornecedores:
-    print(fornecedor.nome, fornecedor.endereco)
-    for item in fornecedor.itens:
-        print("\t", item.nome)
+#fornecedores = session.query(Fornecedor).all()
+#for fornecedor in fornecedores:
+  #  print(fornecedor.nome, fornecedor.endereco)
+  #  for item in fornecedor.itens:
+    #    print("\t", item.nome)
 
-funcionarios = session.query(Funcionario).all()
-for funcionario in funcionarios:
-    print(funcionario.nome, funcionario.cpf, funcionario.cargo)
+#funcionarios = session.query(Funcionario).all()
+#for funcionario in funcionarios:
+  #  print(funcionario.nome, funcionario.cpf, funcionario.cargo)
 
 
 #update
 
-item = session.query(Item).filter_by(nome="Arroz").first()
-item.preco = 600
-session.commit()
+#item = session.query(Item).filter_by(nome="Arroz").first()
+#item.preco = 600
+#session.commit()
 
 
-fornecedor = session.query(Fornecedor).filter_by(nome="Fornecedor A").first()
-fornecedor.endereco = "Rua 456, Bairro D"
-session.commit()
+#fornecedor = session.query(Fornecedor).filter_by(nome="Fornecedor A").first()
+#fornecedor.endereco = "Rua 456, Bairro D"
+#session.commit()
 
 #delete
 
-item = session.query(Item).filter_by(nome="Arroz").first()
-session.delete(item)
-session.commit()
+#item = session.query(Item).filter_by(nome="Arroz").first()
+#session.delete(item)
+#session.commit()
 
 
-fornecedor = session.query(Fornecedor).filter_by(nome='Empresa de Alimentos').first()
-session.delete(fornecedor)
-session.commit()
+#fornecedor = session.query(Fornecedor).filter_by(nome='Empresa de Alimentos').first()
+#session.delete(fornecedor)
+#session.commit()
 
