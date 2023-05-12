3bde9ad7c9af03a0c654d73774bfab0703d483fe
GABRIEL HANSEN DE VASCONCELOS <72347313+Gabriel-Hansen@users.noreply.github.com>
2023-05-12 19:56:45 -0300

    Update Mercado.py
    autoincrement=True no id

--- Mercado.py
+++ Mercado.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 
 class Funcionario(Base):
     __tablename__ = 'funcionarios'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String)
     cpf = Column(String)
     cargo = Column(String)
@@ -17,7 +17,7 @@ class Funcionario(Base):
 
 class Item(Base):
     __tablename__ = 'itens'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String)
     codigo_barras = Column(String)
     preco = Column(Integer)
@@ -27,7 +27,7 @@ class Item(Base):
 
 class Fornecedor(Base):
     __tablename__ = 'fornecedores'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String)
     endereco = Column(String)
     itens = relationship("Item", back_populates="fornecedor")
@@ -36,7 +36,7 @@ class Fornecedor(Base):
 
 class Venda(Base):
     __tablename__ = 'vendas'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     item_id = Column(Integer, ForeignKey('itens.id'))
     item = relationship("Item")
     funcionario_id = Column(Integer, ForeignKey('funcionarios.id'))
